4a901863456b43a5ea0b40d3fd337a34c22dc4b9
TheBrownGuyWhoCanCode <45218659+TheBrownGuyWhoCanCode@users.noreply.github.com>
2018-12-04 15:51:04 -0700

    Add files via upload

---
+++ vape.js
@@ -0,0 +1,19 @@
+function myFunction1() {
+    // When the first button is clicked
+    var x = document.getElementById("div1");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+function myFunction2()  {
+    // When the second button is clicked
+    // Doesn't do anything yet
+}
+
+function myFunction3() {
+    // When the third button is clicked
+    // Doesn't do anything yet
+}
